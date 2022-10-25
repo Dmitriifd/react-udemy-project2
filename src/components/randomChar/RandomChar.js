b9cc1c4d44ffca04335479f16fb9874d690838a9
Dmitriifd <idimka9@gmail.com>
2022-10-25 17:55:10 +0500

    upd показ спиннера при клике на кнопку try it

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -28,12 +28,17 @@ class RandomChar extends Component {
 		this.setState({ char, loading: false })
 	}
 
+	onCharLoading = (char) => {
+		this.setState({ char, loading: true })
+	}
+
 	onError = () => {
 		this.setState({ loading: false, error: true })
 	}
 
 	updateChar = () => {
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+    this.onCharLoading()
 		this.marvelService.getCharacter(id).then(this.onCharLoaded).catch(this.onError)
 	}
 
